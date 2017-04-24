f19e5f1b28b517092d4c9beb720794d36bab93dc
Shivani Angane <shivaniangane@shivaniamac.local>
2017-04-24 00:44:02 -0400

    Adding Angular Template

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
@@ -73,12 +73,13 @@ gulp.task('browser-sync', ['default'], function () {
       'dist/**/*'
    ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: "dist",
-         index: "menu.html"
-      }
-   });
+  browserSync.init(files, {
+    server: {
+      baseDir: 'dist',
+      index: 'index.html'
+    },
+    reloadDelay: 1000
+  });
         // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
 });
